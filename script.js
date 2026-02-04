592ce0897df1df79f17b469ed91bd66ca313239c
Pratham Devatwal <prathamdevatwal0@gmail.com>
2026-02-04 17:19:22 +0530

    test logic added

--- script.js
+++ script.js
@@ -2,3 +2,11 @@ const a1 = 'darkTheme';
 const a2 = 'lightTheme';
 
 console.log(a1, a2);
+
+//logic for the toggle button
+
+const toggleBtn = document.getElementById('toggle-btn');
+
+toggleBtn.addEventListener('click', () => {
+    document.documentElement.setAttribute('data-theme', document.documentElement.getAttribute('data-theme') === 'dark' ? 'light' : 'dark');
+});
